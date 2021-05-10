6635463d59f3a8f401c2ed8809fcccbbcdfd56b1
krzkamgit@wp.pl <krzkamgit@wp.pl>
2021-05-10 14:50:32 +0200

    Add project files.

---
+++ RealWorldApp/RealWorldApp/Pages/HomePage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RealWorldApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapCloseMenu_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapLogout_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapContact_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapCart_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapOrders_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapCartIcon_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ImgMenu_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CvProducts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CvCategories_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
