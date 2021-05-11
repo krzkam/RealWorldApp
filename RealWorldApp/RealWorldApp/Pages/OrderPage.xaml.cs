287ab4493d851442f4744a37a4ca7a9c395f53f0
krzkamgit@wp.pl <krzkamgit@wp.pl>
2021-05-11 15:40:21 +0200

    58 - show-data-inside-order-page

--- RealWorldApp/RealWorldApp/Pages/OrderPage.xaml.cs
+++ RealWorldApp/RealWorldApp/Pages/OrderPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using RealWorldApp.Models;
+using RealWorldApp.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +15,23 @@ namespace RealWorldApp.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class OrderPage : ContentPage
     {
+        public ObservableCollection<OrderByUser> OrdersCollection;
         public OrderPage()
         {
             InitializeComponent();
+            OrdersCollection = new ObservableCollection<OrderByUser>();
+            GetOrderItems();
+        }
+
+        private async void GetOrderItems()
+        {
+            var orders = await ApiService.GetOrdersByUser(Preferences.Get("userId", 0));
+            foreach (var order in orders)
+            {
+                OrdersCollection.Add(order);
+            }
+
+            LvOrders.ItemsSource = OrdersCollection;
         }
 
         private void TapBack_Tapped(object sender, EventArgs e)
